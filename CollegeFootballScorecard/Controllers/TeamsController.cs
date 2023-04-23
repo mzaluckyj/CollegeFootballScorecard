2128c375a00625fdaa0f5d74e04d0f3903b7436f
Michael Zaluckyj <117780170+mzaluckyj@users.noreply.github.com>
2023-04-23 19:58:49 -0400

    Add project files.

---
+++ CollegeFootballScorecard/Controllers/TeamsController.cs
@@ -0,0 +1,18 @@
+﻿using CollegeFootballScorecard.Models;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+
+namespace CollegeFootballScorecard.Controllers
+{
+    public class TeamsController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ CollegeFootballScorecard/Models/XXIIseason.cs
@@ -0,0 +1,6 @@
+﻿namespace CollegeFootballScorecard.Models
+{
+    public class XXIIseason 
+    {
+    }
+}
